d033adf281d9ef22101d8cda4bf3efc6c9363964
N1k1ko <127226899+N1k1ko@users.noreply.github.com>
2024-03-24 18:05:49 +0500

    Initial commit

---
+++ Exsr3/Exsr3.cs
@@ -0,0 +1,116 @@
+﻿//LongCount.longCount("999", "+", "15"); //1014
+class Exsr3
+{
+    static string Add(string num1, string num2, int carry)
+    {
+        if (num1.Length == 0 && num2.Length == 0)
+        {
+            if (carry > 0)
+                return carry.ToString();
+            else
+                return "";
+        }
+
+        int digit1 = num1.Length > 0 ? num1[^1] - '0' : 0;
+        int digit2 = num2.Length > 0 ? num2[^1] - '0' : 0;
+
+        int sum = digit1 + digit2 + carry;
+        carry = sum / 10;
+
+        return Add(num1.Length > 0 ? num1[..^1] : "",
+                    num2.Length > 0 ? num2[..^1] : "",
+                    carry) + (sum % 10).ToString();
+    }
+
+    static string Subtract(string num1, string num2)
+    {
+        if (num1.Length == 0 && num2.Length == 0)
+            return "";
+
+        int digit1 = num1.Length > 0 ? num1[^1] - '0' : 0;
+        int digit2 = num2.Length > 0 ? num2[^1] - '0' : 0;
+
+        int diff = digit1 - digit2;
+
+        if (diff < 0)
+        {
+            int borrowFromIndex = num1.Length - 2;
+
+            while (num1[borrowFromIndex] == '0')
+            {
+                num1 = num1.Remove(borrowFromIndex, 1).Insert(borrowFromIndex, "9");
+                borrowFromIndex--;
+            }
+            num1 = num1.Remove(borrowFromIndex, 1)
+                        .Insert(borrowFromIndex, (num1[borrowFromIndex] - '1')
+                        .ToString());
+            diff = 10 + digit1 - digit2;
+        }
+
+        return Subtract(num1.Length > 0 ? num1[..^1] : "",
+                        num2.Length > 0 ? num2[..^1] : "") + diff.ToString();
+    }
+
+
+    static string PostSubtract(string num1, string num2) //удаляет 0 из начала при вычитании
+    {
+        var result = Subtract(num1, num2);
+        return result[0] == '0' ? result[1..] : result;
+    }
+
+    static string PerformOperation(string num1, string op, string num2)
+    {
+        var ch1 = num1[0] == '-' ? '-' : '+';
+        var ch2 = num2[0] == '-' ? '-' : '+';
+        var len1 = num1.Length;
+        var len2 = num2.Length;
+
+        switch (op)
+        {
+            case "+":
+                {
+                    switch (ch1, ch2)
+                    {
+                        case ('-', '-'): return "-" + Add(num1[1..], num2[1..], 0);
+                        case ('+','+'):  return Add(num1, num2, 0);
+                        case ('-', '+'):
+                        {
+                            if (len1 >= len2 && num1[1] > num2[0])
+                                return "-" + PostSubtract(num1,num2);
+                            else 
+                                return PostSubtract(num1,num2);
+                        }
+                        case ('+','-'): return PostSubtract(num1,num2);
+                    }
+                    break;
+                }
+            case "-":
+                {
+                    switch (ch1, ch2)
+                    {
+                        case ('+', '-'): return Add(num1, num2[1..], 0);
+                        case ('-', '+'): return "-" + Add(num1[1..], num2, 0);
+                        case ('-','-'): return PostSubtract(num1,num2);
+                        case ('+','+'): return PostSubtract(num1,num2);
+                    }
+                    break;
+                }
+        }
+        throw new Exception("Wrong operator");
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        string num1 = Console.ReadLine();
+
+        Console.WriteLine("Enter the operation (+ or -):");
+        string op = Console.ReadLine();
+
+        Console.WriteLine("Enter the second number:");
+        string num2 = Console.ReadLine();
+
+        string result = PerformOperation(num1, op, num2);
+        Console.WriteLine("Result: " + result);
+    }
+}
